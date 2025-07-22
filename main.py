a71a74a887f504b23cc84d40ce599b5f72dc5af9
DaniilShomin <d.sh.v@yandex.ru>
2025-07-22 19:46:39 +0300

    update main.py - add print

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
-    print('Hello from Hexlet!')
+    print("Hello from Hexlet!")
+    print("Hello from Daniil!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
